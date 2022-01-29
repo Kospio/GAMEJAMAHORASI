bd202b4d5375837c930a88a530346750f3d84fac
LuciaFernandez <luciaft2000@hotmail.com>
2022-01-29 03:51:19 +0100

    movement smooth. se para cuando el raycast detecta un bloque delante. va el tp pero hay que revisarle

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,45 +5,47 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     public DimensionHandler dimensionHandler;
-    public float speed = 3f;
+
+    [SerializeField] private float speed = 3f;
+    [SerializeField] private float raycastDistance = 1.4f;
 
     private bool canMove = true;
+    IEnumerator coroutineMovement;
 
-    private void Start()
-    {
-        //StartCoroutine(MovementCoroutine());
-    }
     private void Update()
     {
 
         if (Input.GetKeyDown(KeyCode.D) && canMove) //Swipe right
         {
-            StartCoroutine(MovementCoroutine(speed, 0, Vector3.right));
+            coroutineMovement = MovementCoroutine(speed, 0, Vector3.right);
+            StartCoroutine(coroutineMovement);
         }
         else if (Input.GetKeyDown(KeyCode.A) && canMove) //Swipe left
         {
-            StartCoroutine(MovementCoroutine(-speed, 0, Vector3.left));
+            coroutineMovement = MovementCoroutine(-speed, 0, Vector3.left);
+            StartCoroutine(coroutineMovement);
         }
         else if (Input.GetKeyDown(KeyCode.W) && canMove) //Swipe up
         {
-            StartCoroutine(MovementCoroutine(0, speed, Vector3.forward));
+            coroutineMovement = MovementCoroutine(0, speed, Vector3.forward);
+            StartCoroutine(coroutineMovement);
         }
         else if (Input.GetKeyDown(KeyCode.S) && canMove) //Swipe down
         {
-            StartCoroutine(MovementCoroutine(0, -speed, -Vector3.forward));
+            coroutineMovement = MovementCoroutine(0, -speed, -Vector3.forward);
+            StartCoroutine(coroutineMovement);
         }
     }
 
 
     IEnumerator MovementCoroutine(float xDistance, float zDistance, Vector3 rayDirection)
     {
-
         canMove = false;
         while (true)
         {
             //Raycast
             RaycastHit Rhit;
-            if (Physics.Raycast(transform.position, transform.TransformDirection(rayDirection), out Rhit, 1.4f, dimensionHandler.currentDimension)) // 2 METROS
+            if (Physics.Raycast(transform.position, transform.TransformDirection(rayDirection), out Rhit, raycastDistance, dimensionHandler.currentDimension))
             {
                 if (Rhit.collider.tag == "Bloque") //Hit
                 {
@@ -64,6 +66,10 @@ public class Movement : MonoBehaviour
 
     public void Teleport(GameObject otherTP)
     {
+        StopCoroutine(coroutineMovement);
+        GetComponent<Rigidbody>().velocity = Vector3.zero;
+        canMove = true;
+
         dimensionHandler.ChangeDimension();
 
         otherTP.GetComponent<Collider>().enabled = false;
