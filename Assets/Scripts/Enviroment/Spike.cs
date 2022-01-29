75e29d2d9dff15a3eb8e8099914f621bad5f6e08
LuciaFernandez <luciaft2000@hotmail.com>
2022-01-29 21:00:45 +0100

    blocking level2

--- Assets/Scripts/Enviroment/Spike.cs
+++ Assets/Scripts/Enviroment/Spike.cs
@@ -4,26 +4,54 @@ using UnityEngine;
 
 public class Spike : MonoBehaviour
 {
+    public enum Direction { Side, UpDown}
+    public Direction currentDirection;
+
     public float speed = 1f;
+
+    [Header("SIDE MOVEMENT")]
     public float XrightLimit = 13f;
     public float XleftLimit = 11f;
 
+    [Header("UPDOWN MOVEMENT")]
+    public float ZrightLimit = 13f;
+    public float ZleftLimit = 11f;
 
     void Update()
     {
-        //basic movemenT
-        Vector3 pos = transform.position;
-        pos.x += speed * Time.deltaTime;
-        transform.position = pos;
+        if(currentDirection== Direction.Side)
+        {
+            //basic movemenT
+            Vector3 pos = transform.position;
+            pos.x += speed * Time.deltaTime;
+            transform.position = pos;
 
-        //changing direction
-        if (pos.x < XrightLimit)
-        {
-            speed = Mathf.Abs(speed); //move right
+            //changing direction
+            if (pos.x < XrightLimit)
+            {
+                speed = Mathf.Abs(speed); //move right
+            }
+            else if (pos.x > XleftLimit)
+            {
+                speed = -Mathf.Abs(speed); //move left
+            }
         }
-        else if (pos.x > XleftLimit)
+        else if (currentDirection == Direction.UpDown)
         {
-            speed = -Mathf.Abs(speed); //move left
+            //basic movemenT
+            Vector3 pos = transform.position;
+            pos.z += speed * Time.deltaTime;
+            transform.position = pos;
+
+            //changing direction
+            if (pos.z < ZrightLimit)
+            {
+                speed = Mathf.Abs(speed); //move right
+            }
+            else if (pos.z > ZleftLimit)
+            {
+                speed = -Mathf.Abs(speed); //move left
+            }
         }
     }
 }
