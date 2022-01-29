14c2357536d035ea1e56d3d971bdc3088e49fcbd
Kospio <55896197+Kospio@users.noreply.github.com>
2022-01-29 13:57:55 +0100

    GameLogic Start

---
+++ Assets/GameLogic.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameLogic : MonoBehaviour
+{
+    public bool gotAmplifier;
+    public GameObject amplifier; 
+
+    public bool gotTickets;
+    public GameObject tickets; 
+
+    public bool doorUntapped;
+    public GameObject door; 
+    public bool finish; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
