b622a86343b3a4658ba08bd1a5406bacb6313239
LuciaFernandez <luciaft2000@hotmail.com>
2022-01-30 10:48:20 +0100

    pause Menu

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -1,16 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class PauseMenu : MonoBehaviour
 {
-    void Start()
+    public static bool GamePaused = false;
+
+    public AudioMixer audioMixer;
+
+    [Header("UI")]
+    public Canvas pauseCanvas;
+    public Slider volumeSlider;
+
+
+    public void SetVolume()
     {
-        
+        audioMixer.SetFloat("MusicVolume", volumeSlider.value);
     }
 
-    void Update()
+    private void Start()
     {
-        
+        pauseCanvas.enabled = false;
     }
+
+    public void OpenClose()
+    {
+        GamePaused = GamePaused ? GamePaused = false : GamePaused = true;
+        TryShowMenu();
+    }
+    private void TryShowMenu()
+    {
+        if (GamePaused) //Poner pausa
+        {
+            Time.timeScale = 0f;
+            pauseCanvas.enabled = true;
+        }
+        else //Quitar pausa
+        {
+            Time.timeScale = 1f;
+            pauseCanvas.enabled = false;
+        }
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+
 }
